f1b37a4cebbb99ece946105b778da15168fd384f
sewwandi789 <117725276+sewwandi789@users.noreply.github.com>
2023-07-26 15:28:02 +0530

    Add files via upload
    Birthday wishes form. Blinking label every 0.5 millisecond.

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace wpapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label15 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label2.Location = new System.Drawing.Point(30, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "H";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label3.Location = new System.Drawing.Point(94, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 30);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "P";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label4.Location = new System.Drawing.Point(64, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 30);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "A";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label5.Location = new System.Drawing.Point(220, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 30);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "B";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label6.Location = new System.Drawing.Point(156, 82);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 30);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label7.Location = new System.Drawing.Point(125, 72);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(29, 30);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "P";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label8.Location = new System.Drawing.Point(279, 72);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(29, 30);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "R";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label9.Location = new System.Drawing.Point(254, 97);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 30);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "I";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label10.Location = new System.Drawing.Point(311, 97);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(29, 30);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "T";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label11.Location = new System.Drawing.Point(342, 72);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(32, 30);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "H";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label12.Location = new System.Drawing.Point(376, 97);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(30, 30);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "D";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label13.Location = new System.Drawing.Point(419, 82);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(28, 30);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "A";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Showcard Gothic", 18F);
+            this.label14.Location = new System.Drawing.Point(449, 97);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(30, 30);
+            this.label14.TabIndex = 14;
+            this.label14.Text = "Y";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(82, 31);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(0, 13);
+            this.label15.TabIndex = 15;
+            this.label15.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 365);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label15;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace wpapp
+{
+    public partial class Form1 : Form
+    {
+        int  tickCount = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void lblmethod() 
+        {
+
+            label2.Visible = true;
+            label4.Visible = true;
+            label3.Visible = true;
+            label7.Visible = true;
+            label6.Visible = true;
+            label5.Visible = true;
+            label9.Visible = true;
+            label8.Visible = true;
+            label10.Visible = true;
+            label11.Visible = true;
+            label12.Visible = true;
+            label13.Visible = true;
+            label14.Visible = true;
+        
+        }
+        private void lblmethoddntshow()
+        {
+
+            label2.Visible = false;
+            label4.Visible = false;
+            label3.Visible = false;
+            label7.Visible = false;
+            label6.Visible = false;
+            label5.Visible = false;
+            label9.Visible = false;
+            label8.Visible = false;
+            label10.Visible = false;
+            label11.Visible = false;
+            label12.Visible = false;
+            label13.Visible = false;
+            label14.Visible = false;
+
+        }
+        private void blinkingMethod() 
+        {
+         
+            tickCount++;             
+
+            if (tickCount % 10 == 0) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod(); label4.Visible = false; label13.Visible = false;
+                
+            }
+            if (tickCount % 10 == 1) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label4.Visible = false;label14.Visible = false;
+            }
+            if (tickCount % 10 == 2) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label3.Visible = false;
+            }
+            if (tickCount % 10 == 3) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label7.Visible = false;              
+            }
+            if (tickCount % 10 == 4) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label6.Visible = false;               
+            }
+            if (tickCount % 10 == 5) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label12.Visible = false;
+            }
+            if (tickCount % 10 == 6) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label9.Visible = false;     
+            }
+            if (tickCount % 10 == 7) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label8.Visible = false;                
+            }
+            if (tickCount % 10 == 8) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label10.Visible = false;                
+            }
+            if (tickCount % 10 == 9) // Execute a specific action every 10 ticks (10 seconds).
+            {
+                lblmethod();label11.Visible = false;               
+            }
+          
+         }     
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Interval = 100; // Initial interval set to 1 second (1000 milliseconds).
+            timer1.Start();
+            timer1.Tick += timer1_Tick;
+            lblmethoddntshow();
+
+            string bdstatus = "";
+            DateTime date = DateTime.Now;
+
+           // if (date == "") { }
+            string sql = "";
+
+            //timer2.Interval = 500; // Initial interval set to 0.5 second (1000 milliseconds).
+            //timer1.Start();
+            //timer2.Tick += timer2_Tick; 
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            blinkingMethod();
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
